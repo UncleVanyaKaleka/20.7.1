e00132331cb1c1f5d9ced463e246d69c713a3073
UncleVanyaKaleka <137908775+UncleVanyaKaleka@users.noreply.github.com>
2023-09-07 21:33:19 +0300

    Add files via upload

---
+++ Cat.py
@@ -0,0 +1,17 @@
+class Cat1:
+    def __init__(self, name, gender, breed, age, status):
+        self.name = name
+        self.gender = gender
+        self.breed = breed
+        self.age = age
+        self.status = status
+    def getName(self):
+        return self.name
+    def getGender(self):
+        return self.gender
+    def getBreed(self):
+        return self.breed
+    def getAge(self):
+        return self.age
+    def getStatus(self):
+        return self.status
